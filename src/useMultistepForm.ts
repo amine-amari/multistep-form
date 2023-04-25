f69b56814e5ae1ed1496f90313eeacb5f9974c0e
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-04-25 17:52:18 +0200

    add multistepform custom hook logic

---
+++ src/useMultistepForm.ts
@@ -0,0 +1,33 @@
+import { ReactElement, useState } from "react";
+
+function useMultistepForm(steps: ReactElement[]) {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+
+  function next() {
+    setCurrentStepIndex(prev => {
+        if (prev >= steps.length - 1) return prev
+        return prev + 1
+    })
+  }
+
+  function back() {
+    setCurrentStepIndex(prev => {
+        if (prev <= 0) return prev
+        return prev - 1
+    })
+  }
+
+  function goTo(index: number) {
+    setCurrentStepIndex(index)
+  }
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    goTo,
+    next,
+    back
+  }
+}
+
+export default useMultistepForm
